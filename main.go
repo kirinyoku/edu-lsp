c73cd2bb91d6eb59befe26f530ffa61f1ab0e737
Kirin <rudenokaleksandr21@gmail.com>
2025-01-10 13:06:35 +0200

    add basic handling of client messages

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"os"
 
+	"github.com/kirinyoku/lsp/lsp"
 	"github.com/kirinyoku/lsp/rpc"
 )
 
@@ -16,14 +18,48 @@ func main() {
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(logger, msg)
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("Failed to decode message: %v\n", err)
+			continue
+		}
+
+		handleMessage(logger, method, contents)
 	}
 }
 
-func handleMessage(logger *log.Logger, msg any) {
-	// TODO: Implement message handling
-	logger.Println(msg)
+func handleMessage(logger *log.Logger, method string, contents []byte) {
+	const op = "handleMessage"
+
+	logger.Printf("Received message wtih method: %s\n", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("%s: Failed to unmarshal initialize request: %v\n", op, err)
+			return
+		}
+
+		logger.Printf("Conected to: %s %s\n", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+
+		response := lsp.NewInitializeResponse(request.ID)
+		reply := rpc.MustEncodeMessage(response)
+
+		writer := os.Stdout
+		writer.Write([]byte(reply))
+
+		logger.Println("Sent the reply")
+	case "textDocument/didOpen":
+		var request lsp.DidOpenTextDocumentNotification
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("%s: Failed to unmarshal didOpen request: %v\n", op, err)
+			return
+		}
+
+		logger.Printf("Opened: %s %s\n", request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+	}
 }
 
 func mustGetLogger(filename string) *log.Logger {
